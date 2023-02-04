4910e34aaea74dfaa054b925e8e407e18fc1569c
Eynav Ptia <onevptaya@gmail.com>
2023-02-04 15:45:35 +0200

    configure routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
     <div>
-      <ListEmployeeComponent />
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element = {<ListEmployeeComponent/>}></Route>
+            <Route exact path='/employees' element = {<ListEmployeeComponent/>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div>
+                    Employee Managment Application
+                </div>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
